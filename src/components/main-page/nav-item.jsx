30b54fdc3c75932d9b1711eb01d06d5b71e70cb8
verzynx <verzynx@gmail.com>
2024-01-04 15:53:50 +0700

    style: fix chevron bg for darkmode

--- src/components/main-page/nav-item.jsx
+++ src/components/main-page/nav-item.jsx
@@ -93,7 +93,7 @@ export default function NavItem({
       {!!id && (
         <div
           role="button"
-          className="mr-1 h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600"
+          className="mr-1 h-full rounded-sm hover:bg-neutral-300 hover:dark:bg-neutral-600"
           onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50 " />
