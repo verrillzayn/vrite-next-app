80b0da3a19c942db12918ffe5a02e48cc6ca28e2
verzynx <verzynx@gmail.com>
2024-01-04 10:25:28 +0700

    chore: remove unnecesary  import

--- src/components/main-page/nav-item.jsx
+++ src/components/main-page/nav-item.jsx
@@ -17,7 +17,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
