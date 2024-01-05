f05fbe1d45a4e35e9308725ad42168645e4e3315
verzynx <verzynx@gmail.com>
2024-01-05 15:40:41 +0700

    style: fixing icon width

--- src/components/main-page/nav-item.jsx
+++ src/components/main-page/nav-item.jsx
@@ -103,7 +103,7 @@ export default function NavItem({
       {documentIcon ? (
         <div className="mr-2 shrink-0 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className="mr-2 h-[18px] shrink-0 text-muted-foreground" />
+        <Icon className="mr-2 h-[18px] w-[18px] shrink-0 text-muted-foreground" />
       )}
       <span className="truncate">{label}</span>
       {isSearch && (
